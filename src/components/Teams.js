d5920c3e75264a97c332cd6c2e73aa8d4154d1d6
Yago Gonzalez <jagogc@gmail.com>
2018-06-02 21:31:51 +0100

    Rendering Teams

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import { getTeamNames } from "./../api";
+import TeamLogo from "./TeamLogo";
+import Team from "./Team";
 
 export default class Teams extends Component {
   state = {
@@ -18,6 +21,7 @@ export default class Teams extends Component {
   render() {
     const { loading, teamNames } = this.state;
     const { location, match } = this.props;
+
     return (
       <div className="container two-column">
         <Sidebar
@@ -26,9 +30,47 @@ export default class Teams extends Component {
           list={teamNames}
           {...this.props}
         />
+
         {loading === false && location.pathname === "/teams" ? (
           <div className="sidebar-instruction">Select a Team</div>
         ) : null}
+
+        <Route
+          path={`${match.url}/:teamId`}
+          render={({ match }) => (
+            <div className="panel">
+              <Team id={match.params.teamId}>
+                {team =>
+                  team === null ? (
+                    <h1>Loading</h1>
+                  ) : (
+                    <div style={{ width: "100%" }}>
+                      <TeamLogo id={team.id} className="center" />
+                      <h1 className="medium-header">{team.name}</h1>
+                      <ul className="info-list row">
+                        <li>
+                          Established<div>{team.established}</div>
+                        </li>
+                        <li>
+                          Manager<div>{team.manager}</div>
+                        </li>
+                        <li>
+                          Coach<div>{team.coach}</div>
+                        </li>
+                      </ul>
+                      <Link
+                        className="center btn-main"
+                        to={`/${match.params.teamId}`}
+                      >
+                        {team.name} Team Page
+                      </Link>
+                    </div>
+                  )
+                }
+              </Team>
+            </div>
+          )}
+        />
       </div>
     );
   }
